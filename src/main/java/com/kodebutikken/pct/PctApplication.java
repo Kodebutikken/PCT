77188d9cb32adc6c0bc6441e99c02d0de4125e34
Walterb0b <osca0001@stud.ek.dk>
2026-05-05 08:57:00 +0200

    initialize Spring Boot application with Maven wrapper and basic project structure

---
+++ src/main/java/com/kodebutikken/pct/PctApplication.java
@@ -0,0 +1,13 @@
+package com.kodebutikken.pct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PctApplication.class, args);
+    }
+
+}
