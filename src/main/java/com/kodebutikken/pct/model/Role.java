d33d5f880ba9eb30b68ef94041e09e2cf2a80b1a
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-05-06 08:43:26 +0200

    add User and Role classes for user management

---
+++ src/main/java/com/kodebutikken/pct/model/Role.java
@@ -0,0 +1,6 @@
+package com.kodebutikken.pct.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/kodebutikken/pct/model/User.java
@@ -0,0 +1,52 @@
+package com.kodebutikken.pct.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private Role role;
+
+    public User (int id, String name, String email, String passwordHash, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail(String email) {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
