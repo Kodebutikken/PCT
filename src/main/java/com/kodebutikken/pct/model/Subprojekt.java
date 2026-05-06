aab3c0dce3cfaf7d96daeb2ac56a75c51d42726e
Zander3-2 <zaen0001@stud.ek.dk>
2026-05-06 09:21:21 +0200

    Added Subprojekt model class with fields, constructor and @Data

---
+++ src/main/java/com/kodebutikken/pct/model/Subprojekt.java
@@ -0,0 +1,31 @@
+package com.kodebutikken.pct.model;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Data
+
+public class Subprojekt {
+    private int id;
+    private String titel;
+    private String beskrivelse;
+    private LocalDate deadline;
+    private int projectId;
+    private LocalDateTime createdAt;
+
+    public Subprojekt(int id, String titel, String beskrivelse, LocalDate deadline, int projectId, LocalDateTime createdAt) {
+        this.id = id;
+        this.titel = titel;
+        this.beskrivelse = beskrivelse;
+        this.deadline = deadline;
+        this.projectId = projectId;
+        this.createdAt = createdAt;
+    }
+
+    public Subprojekt() {
+    }
+
+}
+
+
+
