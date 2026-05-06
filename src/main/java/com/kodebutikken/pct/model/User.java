6b69fe1c147bf469fcaa584143ce40222fe4efdd
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-05-06 08:53:30 +0200

    Lombok

--- src/main/java/com/kodebutikken/pct/model/User.java
+++ src/main/java/com/kodebutikken/pct/model/User.java
@@ -1,5 +1,8 @@
 package com.kodebutikken.pct.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private int id;
     private String name;
@@ -12,41 +15,6 @@ public class User {
         this.name = name;
         this.email = email;
         this.passwordHash = passwordHash;
-        this.role = role;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail(String email) {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-    public void setRole(Role role) {
         this.role = role;
     }
 }
